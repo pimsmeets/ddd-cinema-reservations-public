e555fc2a04fa2b3c274ae66203efff0426af9025
Paul de Raaij <pderaaij@xebia.com>
2020-04-21 07:29:23 +0200

    Introduce maximum of 8 tickets rule

--- CinemaReservations/CSharp/CinemaReservations.Domain/MovieScreening.cs
+++ CinemaReservations/CSharp/CinemaReservations.Domain/MovieScreening.cs
@@ -5,11 +5,17 @@ namespace CinemaReservations.Domain
 {
     public class MovieScreening  {
         public readonly Dictionary<string, Row> Rows;
+        private int MAXIMUM_NUMBER_OF_ALLOWED_TICKETS = 8;
+
         public MovieScreening(Dictionary<String, Row> rows) {
             Rows = rows;
         }
         public SeatsAllocated allocateSeats(AllocateSeats allocateSeats)
         {
+            if (allocateSeats.PartyRequested > MAXIMUM_NUMBER_OF_ALLOWED_TICKETS) {
+                return new TooManyTicketsRequested(allocateSeats.PartyRequested);
+            }
+
             var allocation = new SeatAllocation(allocateSeats.PartyRequested);
 
             foreach (var row in Rows)
--- CinemaReservations/CSharp/CinemaReservations.Tests/TicketBoothShould.cs
+++ CinemaReservations/CSharp/CinemaReservations.Tests/TicketBoothShould.cs
@@ -39,5 +39,36 @@ namespace CinemaReservations.Tests
             Check.That(seatsAllocated).IsInstanceOf<NoPossibleAllocationsFound>();
         }
 
+        [Test]
+        public void Reserve_first_possibility_for_multiple_seats_when_available()
+        {
+            const string showId = "3";
+            const int partyRequested = 2;
+
+            IMovieScreeningRepository repository =  new StubMovieScreeningRepository(new StubAuditoriumRepository());
+            TicketBooth ticketBooth = new TicketBooth(repository);
+
+            var seatsAllocated = ticketBooth.AllocateSeats(new AllocateSeats(showId, partyRequested));
+
+            Check.That(seatsAllocated.ReservedSeats).HasSize(2);
+            Check.That(seatsAllocated.ReservedSeats[0].ToString()).IsEqualTo("A6");
+            Check.That(seatsAllocated.ReservedSeats[1].ToString()).IsEqualTo("A7");
+        }
+
+        [Test]
+        public void Return_TooManyTicketsRequested_when_9_tickets_are_requested()
+        {
+            const string showId = "5";
+            const int partyRequested = 9;
+
+            IMovieScreeningRepository repository =  new StubMovieScreeningRepository(new StubAuditoriumRepository());
+            TicketBooth ticketBooth = new TicketBooth(repository);
+
+            var seatsAllocated = ticketBooth.AllocateSeats(new AllocateSeats(showId, partyRequested));
+
+            Check.That(seatsAllocated.ReservedSeats).HasSize(0);
+            Check.That(seatsAllocated).IsInstanceOf<TooManyTicketsRequested>();
+        }
+
     }
 }
